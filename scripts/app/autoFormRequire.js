f57436e93fe041dd5a6ccafe9803adc48434bfb6
m658761 <bob.warren@macys.com>
2016-06-04 13:51:17 -0700

    working demo

--- scripts/app/autoFormRequire.js
+++ scripts/app/autoFormRequire.js
@@ -32,7 +32,7 @@ require(["augmented", "augmentedPresentation"],
     var schema = {
         "$schema": "http://json-schema.org/draft-04/schema#",
         "title": "User",
-        "description": "A User",
+        "description": "A list of users",
         "type": "object",
         "properties": {
             "Name" : {
@@ -45,19 +45,117 @@ require(["augmented", "augmentedPresentation"],
             },
             "Email" : {
                 "description": "The email of the user",
-                "type" : "string"
+                "type" : "string",
+                "format": "email"
+            },
+            "Role" : {
+                "description": "The role of the user",
+                "type": "string",
+                "enum": ["Architect", "Developer", "Tech Lead", "Product Owner", "UX Designer", "QE", "Operations"]
+            },
+            "Active" : {
+                "description": "Is the user Active",
+                "type" : "boolean"
             }
         },
-        "required": ["ID", "Name"]
+        "required": ["Name", "ID"]
     };
+
+    var User = Augmented.Model.extend({
+        defaults: {
+            "Name": "",
+            "ID": 0,
+            "Email": "",
+            "Role": "Developer",
+            "Active": false
+        }
+    });
+
+    var UserCollection = Augmented.Collection.extend({
+        model: User
+    });
+
+    var MyMediator = Augmented.Presentation.Mediator.extend({
+        el: "#mediator",
+        init: function(options) {
+            this.on("add", function() {
+                //this.publish("userlist", "add", model);
+                this.publish("form", "publishData");
+            });
+
+            this.on("reset", function() {
+                //this.publish("userlist", "add", model);
+                this.publish("form", "reset");
+            });
+
+            this.on("publishData", function(model) {
+                this.publish("userlist", "add", model);
+            });
+        },
+        render: function() {
+        }
+    });
 
     var MyForm = Augmented.Presentation.AutomaticForm.extend({
         el: "#main",
-        schema: schema
+        schema: schema,
+        init: function() {
+            this.on("publishData", function() {
+                this.sendMessage("publishData", this.model.toJSON());
+            });
+
+            this.on("reset", function() {
+                this.reset();
+            });
+        }
     });
 
+    var ControlPanel = Augmented.Presentation.DecoratorView.extend({
+        name: "control",
+        el: "#control",
+        add: function() {
+            this.sendMessage("add", "add");
+        },
+        reset: function() {
+            this.sendMessage("reset", "reset");
+        }
+    });
+
+    var UserList = Augmented.Presentation.Colleague.extend({
+        el: "#users",
+        init: function() {
+            this.collection = new UserCollection();
+
+            this.on("add", function(model) {
+                this.addUser(model);
+                this.render();
+            });
+        },
+        render: function() {
+            if (!this.collection.isEmpty()) {
+                var i = 0, l = this.collection.length, el = Augmented.Presentation.Dom.selector("#users > #userList"), list;
+                Augmented.Presentation.Dom.empty(el);
+                for (i = 0; i < l; i++) {
+                    list = Augmented.Presentation.Widget.DescriptionList(null, this.collection.at(i).toJSON());
+                    el.appendChild(list);
+                }
+            }
+        },
+        addUser: function(model) {
+            var user = new User(model);
+            this.collection.add(user);
+        }
+    });
+
+    var mediator = new MyMediator();
     var formView = new MyForm();
+    var control = new ControlPanel();
+    var userList = new UserList();
+
     formView.render();
+    userList.render();
 
-
+    mediator.observeColleagueAndTrigger(formView, "form");
+    mediator.observeColleagueAndTrigger(control, "control");
+    mediator.observeColleagueAndTrigger(userList, "userlist");
 });
